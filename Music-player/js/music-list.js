0c082377f62d3fe0b45d70a6004d6e3f80b1521c
Deepak Chowdry <113930530+DeepakChowdry-bit@users.noreply.github.com>
2022-11-15 22:16:03 +0500

    main

---
+++ Music-player/js/music-list.js
@@ -0,0 +1,43 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  
+  {
+    name: "Jo Tenu Dhoop",
+    artist: "Rito",
+    src: "music-7"
+  },
+  {
+    name: "Raatan Laambiyan",
+    artist: "Jubin Nautiyal",
+    src: "music-8"
+  },
+  {
+    name: "Srivalli",
+    artist: "Javed Ali",
+    
+    src: "music-9"
+  },
+  {
+    name: "Kesariya",
+    artist: "Arijit Singh",
+    
+    src: "music-10"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
