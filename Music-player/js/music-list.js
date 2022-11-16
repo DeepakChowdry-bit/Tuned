17034eb84fc58740ac74a196180e5fd0462ee0d9
Deepak Chowdry <113930530+DeepakChowdry-bit@users.noreply.github.com>
2022-11-16 22:04:04 +0500

    Updates and Fixes

--- Music-player/js/music-list.js
+++ Music-player/js/music-list.js
@@ -12,26 +12,70 @@
 let allMusic = [
   
   {
-    name: "Jo Tenu Dhoop",
-    artist: "Rito",
-    src: "music-7"
-  },
-  {
-    name: "Raatan Laambiyan",
-    artist: "Jubin Nautiyal",
-    src: "music-8"
-  },
-  {
-    name: "Srivalli",
-    artist: "Javed Ali",
+    name: "One Dance",
+    artist: "Drake",
     
-    src: "music-9"
+    src: "Baby-Style"
   },
   {
-    name: "Kesariya",
-    artist: "Arijit Singh",
+    name: "Dandelions",
+    artist: "Ruth B",
     
-    src: "music-10"
+    src: "Dandelions"
+  },
+  {
+    name: "Double Take",
+    artist: "Dhruv",
+    
+    src: "Double-Take"
+  },
+  {
+    name: "Heat Waves",
+    artist: "Glass Animals",
+    
+    src: "Heat-Waves"
+  },
+  {
+    name: "Under the Influence",
+    artist: "Chris Brown",
+    
+    src: "Influence"
+  },
+  {
+    name: "Middle of the Night",
+    artist: "Elley Duhé",
+    
+    src: "Middle-Night"
+  },
+  {
+    name: "Snap",
+    artist: "Rosa Linn",
+    
+    src: "Snap"
+  },
+  {
+    name: "Unstoppable",
+    artist: "Sia",
+    
+    src: "Unstoppable"
+  },
+  {
+    name: "Dancing With Your Ghost",
+    artist: "Sasha Alex Sloan",
+    
+    src: "Dancing-Ghost"
+  },
+  {
+    name: "Perfect",
+    artist: "Ed Sheeran",
+    
+    src: "Perfect"
+  },
+  {
+    name: "Until I Found You",
+    artist: "Stephen Sanchez",
+    
+    src: "Until-Found"
   },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
